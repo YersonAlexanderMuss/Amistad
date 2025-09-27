3595f1a7a08ef353d3531a8fcfc80c468220993b
YersonAlexanderMuss <138311574+YersonAlexanderMuss@users.noreply.github.com>
2025-09-26 23:49:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   const textElement = document.getElementById('typed-text');
-  const fullText = "Para ti Leonela";
+  const fullText = "Leonela Tus Flores";
   let i = 0;
   const speed = 100; // Velocidad de escritura (milisegundos por letra)
 
